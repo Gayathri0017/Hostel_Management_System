60ecda8965c4897e6cff908f3cfdcc60ab15750c
Gayathri0017 <gayathri4866@gmail.com>
2025-02-20 19:11:51 +0530

    created admin class and update main class

--- HMS/src/hostels/VistorManagement.java
+++ HMS/src/hostels/VistorManagement.java
@@ -1,7 +1,5 @@
 package hostels;
-
 import java.util.*;
-
 class Visitor {
     String name;
     String inTime;
@@ -14,20 +12,17 @@ class Visitor {
         System.out.print("Enter InTime (HH:MM): ");
         inTime = sc.nextLine();
     }
-
     void exitDetails() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter OutTime (HH:MM) for " + name + ": ");
         outTime = sc.nextLine();
     }
-
     void displayDetails() {
         System.out.println("Visitor Name: " + name);
         System.out.println("InTime: " + inTime);
         System.out.println("OutTime: " + (outTime != null ? outTime : "Not recorded"));
     }
 }
-
 public class HostelVisitorManagement {
     public static void main(String[] args) {
         Visitor visitor = new Visitor();
